c20e06203edf45186143a3fdbcb6588917f5920b
Great Karthick <nk.karthick@gmail.com>
2018-07-08 20:44:18 +0530

    Added customer update functionality temporarly comment the $UI param

--- Kitchen/src/main/resources/static/assets/js/pages/customer_preview.js
+++ Kitchen/src/main/resources/static/assets/js/pages/customer_preview.js
@@ -6,7 +6,7 @@ function getCustomerList() {
 	// which is returned when getting a token and should be used to 
 	// create the url_base.
 	$.ajax({
-		'url' : baseURI + 'customer/get/' + $.urlParam('id') + '?access_token=' + accessToken,
+		'url' : baseURI + 'customer/get/' +'CUST1' + '?access_token=' + accessToken,
 		'type' : 'GET',
 		'contentType' : 'x-www-form-urlencoded',
 		'crossDomain' : true,
@@ -27,12 +27,18 @@ function getCustomerList() {
 function displayCustomer(data){
 	console.log("Data " + JSON.stringify(data));
 	data = data.customer[0];
+	address = data.address;
 	document.getElementById('displayName').innerHTML = data.firstName + ' ' + data.middleName + ' ' + data.lastName
 	document.getElementById('mobileNo').innerHTML = data.mobileNumber;
+	document.getElementById('emailId').innerHTML = data.email;
+	document.getElementById('dateOfBirth').innerHTML = new Date(data.dateOfBirth);
+	document.getElementById('gender').innerHTML = 'No data from service';
+	document.getElementById('address').innerHTML = address.doorNumber+',' + address.street + ', ' + address.area
+	 + ',' + address.city+' ,' + address.state+' . ';
 }
 
 function edit() {
-	window.location.href = kitchenBaseURI + 'edit_customer.html?id=' + $.urlParam('id');
+	window.location.href = kitchenBaseURI + 'edit_customer.html?id=' + 'CUST1';
 }
 
 function deleteData() {

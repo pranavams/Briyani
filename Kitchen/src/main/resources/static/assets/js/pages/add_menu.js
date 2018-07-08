66de55aee7fb6b52cb296307661e1aaec19bf7a5
Pranav <pranavam.s@gmail.com>
2018-07-08 21:33:25 +0530

    Menu Add Completed

---
+++ Kitchen/src/main/resources/static/assets/js/pages/add_menu.js
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+function getJSONData() {
+	jsonObject = {
+		'name' : document.getElementById('item_name').value,
+		'menuName' : document.getElementById('menu_name').value,
+		'description' : document.getElementById('item_descri').value,
+		'price' : document.getElementById('item_price').value
+	};
+
+	console.log("Inside Get JSON Data " + jsonObject);
+	return jsonObject;
+}
+
+function clearItem() {
+	document.getElementById('item_name').value = '';
+	document.getElementById('menu_name').value = '';
+	document.getElementById('item_descri').value = '';
+	document.getElementById('item_price').value = '';
+}
+
+function saveAndCloseMenu() {
+	saveMenu(true);
+}
+
+function saveMenu(toClose) {
+	var jsonObj = getJSONData();
+	$.ajax({
+		"url" : baseURI + 'item/save?access_token=' + accessToken,
+		'type' : 'POST',
+		'contentType' : 'application/json',
+		'crossDomain' : true,
+		'data' : JSON.stringify(jsonObj),
+		'dataType' : 'json',
+		'success' : function(result) {
+			$.gritter.add({
+				class_name : 'gritter-success',
+				title : 'Success!',
+				text : '<p style="font-size: 14px;">Menu Saved successfully!</p>',
+			});
+			clearItem();
+			return result;
+		},
+		'error' : function(XMLHttpRequest, textStatus, errorThrown) {
+			//Process error actions
+			console.log('getMenu - Error: ' + errorThrown + " - " + textStatus);
+			console.log(XMLHttpRequest.status + ' ' +
+				XMLHttpRequest.statusText);
+			$.gritter.add({
+				class_name : 'gritter-error',
+				title : 'Success!',
+				text : '<p style="font-size: 14px;">Menu Not Saved!</p>',
+			});
+			return false;
+		}
+	});
+}

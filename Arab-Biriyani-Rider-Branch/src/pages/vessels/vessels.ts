2e36991f3d4ffdc321d7f72b297928d88626b230
jkarth24 <jkarth24@ford.com>
2018-07-10 22:50:50 +0530

    Revised Manage Vessels module(BranRider) for accepting values from DB

--- Arab-Biriyani-Rider-Branch/src/pages/vessels/vessels.ts
+++ Arab-Biriyani-Rider-Branch/src/pages/vessels/vessels.ts
@@ -1,98 +1,96 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, PopoverController, Popover } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, PopoverController, Popover} from 'ionic-angular';
+
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @IonicPage()
 @Component({
-    selector: 'page-vessels',
-    templateUrl: 'vessels.html'
+  selector: 'page-vessels',
+  templateUrl: 'vessels.html'
 })
 
-export class VesselsPage { 
-    displayType: any = 'today';
-    today: any = [
-        {
-            order: 'PO# 1100011',
-            status: 'ready',
-            branch: 'Little India',
-            quantity: '80',
-            vessels: '3',
-            phone: '+65 6363 5498',
-            address: '414 Baleser Road<br>Singapore 329806'
-        },
-        {
-            order: 'PO# 1100012',
-            status: 'packing',
-            branch: 'Fancy',
-            quantity: '60',
-            vessels: '5',
-            phone: '+65 6363 3413',
-            address: '414 Nasser Road<br>Singapore 456952'
-        },
-        {
-            order: 'PO# 1100013',
-            status: 'packing',
-            branch: 'Little India',
-            quantity: '100',
-            vessels: '7',
-            phone: '+65 6363 5255',
-            address: '414 Rahman Road<br>Singapore 35622'
-        },
-        {
-            order: 'PO# 1100014',
-            status: 'packing',
-            branch: 'Fine Biriyani',
-            quantity: '120',
-            vessels: '10',
-            phone: '+65 6363 6734',
-            address: '414 Elsaan Road<br>Singapore 63425'
-        }
-    ];
-    completed: any = [
-        {
-            order: 'PO# 1100009',
-            status: 'completed',
-            branch: 'Fine Biriyani',
-            quantity: '120',
-            vessels: '10'
-        },
-        {
-            order: 'PO# 1100008',
-            status: 'completed',
-            branch: 'Little India',
-            quantity: '80',
-            vessels: '3'
-        },
-        {
-            order: 'PO# 1100007',
-            status: 'completed',
-            branch: 'Fancy',
-            quantity: '60',
-            vessels: '5'
-        },
-        {
-            order: 'PO# 1100006',
-            status: 'completed',
-            branch: 'Little India',
-            quantity: '100',
-            vessels: '7'
-        }
-    ]
-    constructor(public navCtrl: NavController, public popoverCtrl: PopoverController){}
-    slider(item, type){
-        let index = this.today.indexOf(item);
-        if(this.today[index].sliderMargin == undefined || this.today[index].sliderMargin == '-130px -16px 0'){
-            this.today[index].sliderContent = true;
-            this.today[index].sliderMargin = '0 -16px 0';
-            this.today[index].sliderType = type;
-        }
-        else{
-            this.today[index].sliderMargin = '-130px -16px 0';
-        }
+export class VesselsPage {
+  displayType: any = 'today';
+
+  serviceUrl = "http://localhost:63636/api/v1/order/listOrdersByVesselStatus/";
+  accessToken: string;
+
+  today: any = [];
+  completed: any = []
+  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public http: HttpClient) {}
+
+  ngOnInit() {
+    this.getData("RETURNED");
+  }
+
+  getData(data: string): void {
+    this.restToken()
+      .subscribe(
+      (tokenResponse) => {
+        this.accessToken = tokenResponse.access_token;//ignore
+        this.dataRetrival(data)
+          .subscribe(
+          responseData => {
+            this.today = responseData.order.filter(x => x.vesselStatus.toUpperCase() !== 'RETURNED');
+            this.completed = responseData.order.filter(x => x.vesselStatus.toUpperCase() === 'RETURNED');
+            console.log(responseData);
+          }
+          )
+      });
+  }
+
+  dataRetrival(data: string) {
+    return this.http
+      .get<any[]>(this.serviceUrl + data + "?access_token=" + this.accessToken)
+      .pipe(map(data => data));
+  }
+
+  getAuthToken() {
+    return "Basic " + btoa('arab-briyani-client:devglan-secret');
+  }
+
+  getAuthTokenParameters() {
+    return new HttpParams()
+      .set('username', 'Alex123')
+      .set('password', 'password')
+      .set('grant_type', 'password');
+  }
+
+  restToken() {
+    return this.http
+      .post<any[]>("http://localhost:63636/oauth/token",
+      this.getAuthTokenParameters().toString(), {
+        headers: new HttpHeaders().set('content-type', 'application/x-www-form-urlencoded')
+          .set('authorization', this.getAuthToken())
+          .set('cache-control', 'no-cache')
+      })
+      .pipe(map(token => token));
+  }
+
+  slider(item, type) {
+    let index = this.today.indexOf(item);
+    if (this.today[index].sliderMargin == undefined || this.today[index].sliderMargin == '-130px -16px 0') {
+      this.today[index].sliderContent = true;
+      this.today[index].sliderMargin = '0 -16px 0';
+      this.today[index].sliderType = type;
     }
-    presentPopover(myEvent) {
-        let popover = this.popoverCtrl.create('RiderPopPage');
-        popover.present({
-            ev: myEvent
-        });
+    else {
+      this.today[index].sliderMargin = '-130px -16px 0';
     }
+  }
+
+  getQuantity(item) {
+    let orderQuantity = item.orderDetails
+      .map(x => x.quantity)
+      .reduce((sum, current) => sum + current, 0);
+    return orderQuantity;
+  }
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create('RiderPopPage');
+    popover.present({
+      ev: myEvent
+    });
+  }
 }

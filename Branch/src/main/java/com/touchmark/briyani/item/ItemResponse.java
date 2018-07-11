715a3e88df2774ab1d23ca9d7095e1581f315fba
Pranav <pranavam.s@gmail.com>
2018-07-11 20:55:42 +0530

    Edit Customer Completed

--- Branch/src/main/java/com/touchmark/briyani/item/ItemResponse.java
+++ Branch/src/main/java/com/touchmark/briyani/item/ItemResponse.java
@@ -2,9 +2,6 @@ package com.touchmark.briyani.item;
 
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- Kitchen/src/main/resources/static/assets/js/pages/edit_customer.js
+++ Kitchen/src/main/resources/static/assets/js/pages/edit_customer.js
@@ -5,7 +5,7 @@ function getCustomerList() {
 	// The baseURI variable is created by the result.base_server_base_uri 
 	// which is returned when getting a token and should be used to 
 	// create the url_base.
-	
+
 	$.ajax({
 		'url' : baseURI + 'customer/get/' + $.urlParam('id') + '?access_token=' + accessToken,
 		'type' : 'GET',
@@ -25,37 +25,34 @@ function getCustomerList() {
 	});
 }
 
-function displayCustomer(data){
+function displayCustomer(data) {
 	console.log("Data " + JSON.stringify(data));
-	data = data.customer[0];	
+	data = data.customer[0];
 	address = data.address;
 	document.getElementById('customer_fname').value = data.firstName;
 	document.getElementById('customer_mname').value = data.middleName;
 	document.getElementById('customer_lname').value = data.lastName;
 	document.getElementById('customer_email').value = data.email;
-	if (data.telephoneNumber !=null) {
+	if (data.telephoneNumber != null) {
 		document.getElementById('customer_telephone_no').value = data.telephoneNumber;
-	}
-	else {
+	} else {
 		document.getElementById('customer_telephone_no').value = "";
 	}
 	setTheGenderValue(data.gender);
 	document.getElementById('customer_mobile_no').value = data.mobileNumber;
 	document.getElementById('customer_dob').value = stringToDate(data.dateOfBirth, 'dd/mm/yyyy', '-')	;
-	document.getElementById('cust_delivery_address').innerHTML = address.doorNumber +' ' + address.street + ' ' + address.area
-	 + ' ' + address.city+' ' + address.state+' ' + address.country + ' ' + address.zipcode;
+	document.getElementById('cust_delivery_address').innerHTML = address.doorNumber + ' ' + address.street + ' ' + address.area
+	+ ' ' + address.city + ' ' + address.state + ' ' + address.country + ' ' + address.zipcode;
 }
 
 function setTheGenderValue(gender) {
-	if (gender!=null && gender!='') {
-		if (gender=='Female'){
+	if (gender != null && gender != '') {
+		if (gender == 'Female') {
 			document.getElementById('customRadioInline2').checked = 'True';
-		
-		}
-		else if (gender=='Male') {
+
+		} else if (gender == 'Male') {
 			document.getElementById('customRadioInline1').checked = 'True';
-		}
-		else {
+		} else {
 			document.getElementById('customRadioInline3').checked = 'True';
 		}
 	}
@@ -70,5 +67,74 @@ function deleteData() {
 	console.log("Delete To Implement");
 }
 
+function clearItem() {
+	document.getElementById('customer_fname').value = '';
+	document.getElementById('customer_mname').value = '';
+	document.getElementById('customer_lname').value = '';
+	document.getElementById('customer_email').value = '';
+	document.getElementById('customer_telephone_no').value = '';
+	document.getElementById('customer_mobile_no').value = '';
+	document.getElementById('customer_dob').value = '';
+	document.getElementById('cust_delivery_address').value = '';
+}
+
+function getJSONUpdateData() {
+	jsonObject = {
+		'id' : $.urlParam('id'),
+		'firstName' : document.getElementById('customer_fname').value,
+		'middleName' : document.getElementById('customer_mname').value,
+		'lastName' : document.getElementById('customer_lname').value,
+		'telephoneNumber' : document.getElementById('customer_telephone_no').value,
+		'mobileNumber' : document.getElementById('customer_mobile_no').value,
+		'email' : document.getElementById('customer_email').value,
+		'gender' : $('input[name=customRadioInline1]:checked').val(),
+		'dateOfBirth' : stringToDate(document.getElementById('customer_dob').value, 'dd/mm/yyyy', '/'),
+		'address' : {
+			'area' : document.getElementById('cust_delivery_address').value
+		}
+	};
+
+	console.log("Inside Get JSON Data " + jsonObject);
+	return jsonObject;
+}
+
+function update() {
+	console.log("Inside Update Customer");
+
+	var jsonObj = getJSONUpdateData();
+
+	var url_base = baseURI;
+	//accessToken = getToken();
+	$.ajax({
+		'url' : baseURI + 'customer/update',
+		'type' : 'POST',
+		'contentType' : 'application/json',
+		'crossDomain' : true,
+		'data' : JSON.stringify(jsonObj),
+		'dataType' : 'json',
+		'success' : function(result) {
+			$.gritter.add({
+				class_name : 'gritter-success',
+				title : 'Success!',
+				text : '<p style="font-size: 14px;">Customer Saved successfully!</p>',
+			});
+			clearItem();
+			return result;
+		},
+		'error' : function(XMLHttpRequest, textStatus, errorThrown) {
+			//Process error actions
+			console.log('getMenu - Error: ' + errorThrown + " - " + textStatus);
+			console.log(XMLHttpRequest.status + ' ' +
+				XMLHttpRequest.statusText);
+			$.gritter.add({
+				class_name : 'gritter-error',
+				title : 'Success!',
+				text : '<p style="font-size: 14px;">Customer Not Saved!</p>',
+			});
+			return false;
+		}
+	});
+
+}
 
 getToken(getCustomerList);

0b4810993d11f95b1b3f39b155bf25681c059143
jkarth24 <jkarth24@ford.com>
2018-07-13 01:23:09 +0530

    Updated order status in track order module

--- Arab-Biriyani-Branch-Manager/src/pages/track-order/track.ts
+++ Arab-Biriyani-Branch-Manager/src/pages/track-order/track.ts
@@ -1,5 +1,8 @@
 import {Component, ViewChild} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Api} from '../../providers/api/api';
 
 @IonicPage()
 @Component({
@@ -9,11 +12,20 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 export class TrackPage {
 
+  trackOrderUrl = this.api.url + "/api/v1/order/get/";
+  accessToken: string;
   order: any;
+  orderStatus: any = [];
+  tempArr: any = this.orderStatus;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public api: Api, public navCtrl: NavController, public navParams: NavParams, private http: HttpClient) {
     this.order = this.navParams.data.items;
   }
+  
+  ngOnInit() {
+    this.getData(this.order.orderId);
+  }
+
 
   backButtonClick() {
     this.navCtrl.pop();
@@ -23,4 +35,48 @@ export class TrackPage {
     this.navCtrl.push('OrderPage');
   }
   
+  getData(paymentStatus: string): void {
+    //this.restToken()
+    //  .subscribe(
+    //  (tokenResponse) => {
+    //    this.accessToken = tokenResponse.access_token; 
+       this.retrieveStatus(this.order.orderId)
+          .subscribe(
+          orderStatus => {
+            this.orderStatus = orderStatus['order'];
+            this.tempArr = orderStatus['order'];
+            console.log("order status: " + this.orderStatus[0].orderStatus);
+          }
+          );
+    //  });
+  }
+
+  retrieveStatus(orderId: number) {
+    return this.http
+      .get<any[]>(this.trackOrderUrl + orderId +"?access_token=" + this.accessToken)
+      .pipe(map(data => data));
+  }
+
+  getAuthToken() {
+    return "Basic " + btoa('arab-briyani-client:devglan-secret');
+  }
+
+  getAuthTokenParameters() {
+    return new HttpParams()
+      .set('username', 'Alex123')
+      .set('password', 'password')
+      .set('grant_type', 'password');
+  }
+
+  restToken() {
+    return this.http
+      .post<any[]>("/oauth/token",
+      this.getAuthTokenParameters().toString(), {
+        headers: new HttpHeaders().set('content-type', 'application/x-www-form-urlencoded')
+          .set('authorization', this.getAuthToken())
+          .set('cache-control', 'no-cache')
+      })
+      .pipe(map(token => token));
+  }
+
 }
